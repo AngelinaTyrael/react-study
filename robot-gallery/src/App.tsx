9a41f66c262befa435e522a84ea5e5bc5bef197b
AngelinaTyrael <lucky18209714890@gmail.com>
2022-04-06 21:58:18 +0800

    react-hook

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import robots from './mockdata/robots.json';
+import React, { useState } from 'react';
 import logo from './assets/images/logo.svg';
 import Robot from './components/Robot'
+import ShoppingCart from './components/ShoppingCart';
 import styles from './App.module.css'
 
-function App() {
+interface Props {
+
+}
+
+interface State {
+  robotGallery: any[],
+  count: number 
+}
+
+const App: React.FC = (props) =>  {
+  const [count, setCount] = useState<number>(0)
+
   return (
     <div className={styles.app}>
       <div className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt="logo" />
         <h1>robots</h1>
       </div>
-      <div className={styles.robotList}>
-        {robots.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
-      </div>
+      <button 
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Click
+      </button>
+      <span>count: { count}</span>
+      <ShoppingCart />
+      {/* <div className={styles.robotList}>
+        {this.state.robotGallery.map(r => <Robot key={r.id} id={r.id} email={r.email} name={r.name} />)}
+      </div> */}
     </div>
   );
 }
