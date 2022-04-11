e46d41782cc00e5352293106b781936811abb06f
AngelinaTyrael <lucky18209714890@gmail.com>
2022-04-11 21:55:05 +0800

    react-hooks

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,20 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/images/logo.svg';
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart';
 import styles from './App.module.css'
 
 interface Props {
-
 }
 
 interface State {
   robotGallery: any[],
-  count: number 
+  count: number
 }
 
-const App: React.FC = (props) =>  {
+const App: React.FC<Props> = (props) => {
   const [count, setCount] = useState<number>(0)
+  const [robotGallery, setRobotGallery] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>()
+
+  useEffect(() => {
+    document.title = `点击次数为${count}次`
+  }, [count])
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setRobotGallery(data)
+      } catch (err: any) {
+        setError(err.msg)
+      }
+      setLoading(false)
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <div className={styles.app}>
@@ -22,18 +45,25 @@ const App: React.FC = (props) =>  {
         <img src={logo} className={styles.appLogo} alt="logo" />
         <h1>robots</h1>
       </div>
-      <button 
+      <button
         onClick={() => {
           setCount(count + 1)
         }}
       >
         Click
       </button>
-      <span>count: { count}</span>
+      <span>count: {count}</span>
       <ShoppingCart />
-      {/* <div className={styles.robotList}>
-        {this.state.robotGallery.map(r => <Robot key={r.id} id={r.id} email={r.email} name={r.name} />)}
-      </div> */}
+      {(!error || error !== '') ? '' : <div>网站出错：{error}</div>}
+      {
+        !loading ?
+          (<div className={styles.robotList}>
+            {robotGallery.map(r => <Robot key={r.id} id={r.id} email={r.email} name={r.name} />)}
+          </div>)
+          :
+          (<h2>loading</h2>)
+      }
+
     </div>
   );
 }
